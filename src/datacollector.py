6edde385082f9e1018879d79987d7e3d8cfebe7b
sezgink <sezginkutlu00@gmail.com>
2023-05-20 12:48:14 +0300

    Getting tweet statistics

--- src/datacollector.py
+++ src/datacollector.py
@@ -1,5 +1,6 @@
 import webdriver
 from selenium.webdriver.common.by import By
+
 import time
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -7,18 +8,34 @@ from selenium.webdriver.chrome.webdriver import WebDriver
 import datetime
 import pandas as pd
 
+import selenium.webdriver.common
+
 driver = webdriver.GetWebdriver()
     
 date_for_deletion = '2022-05-21'
 stop_condition = 5
-
+def CountFromText(countText:str):
+    countText = countText.replace(",","")
+    multiplier = 1
+    for c in countText:
+        if(c=='K'):
+            multiplier=1000
+        elif(c=='M'):
+            multiplier=1000000
+    countText = countText.replace("K","")
+    countText = countText.replace("M","")
+    count =float(countText)
+    count*=multiplier
+    return int(count)
 def ScrapeTweets(driver: WebDriver,stop_condition):
     first_tweet = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div[data-testid=cellInnerDiv]")))
     driver.execute_script('arguments[0].scrollIntoView({block: "center", behavior: "smooth"});', first_tweet)
     time.sleep(1)
     foundTweetCount=0
 
-    column_names = ['username', 'tweet_text', 'tweet_date']
+    # column_names = ['username', 'tweet_text', 'tweet_date']
+    column_names = ['username', 'tweet_text', 'tweet_date','tweet_like_count','tweet_reply_count','tweet_retweet_count']
+
     tweets_df = pd.DataFrame(columns=column_names)
     for i in range(999):
         tweets = driver.find_elements(By.CSS_SELECTOR, 'div[data-testid=cellInnerDiv]')
@@ -32,6 +49,21 @@ def ScrapeTweets(driver: WebDriver,stop_condition):
                 senderName = tweets[0].find_element(By.CSS_SELECTOR, 'div[data-testid=User-Name]')
                 senderName = senderName.find_element(By.CSS_SELECTOR,'div[class="css-901oao css-1hf3ou5 r-14j79pv r-18u37iz r-37j5jr r-1wvb978 r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-qvutc0"]')
                 statusLink = tweets[0].find_element(By.XPATH, './/time').find_element(By.XPATH,"..").get_attribute("href")
+
+                fetched_statistics = False
+                try:
+                    replyCount = tweets[0].find_element(By.CSS_SELECTOR, 'div[data-testid=reply]').text
+                    replyCountNum = CountFromText(replyCount)
+                    retweetCount = tweets[0].find_element(By.CSS_SELECTOR, 'div[data-testid=retweet]').text
+                    retweetCountNum = CountFromText(retweetCount)
+                    likeCount = tweets[0].find_element(By.CSS_SELECTOR, 'div[data-testid=like]').text
+                    likeCountNum = CountFromText(likeCount)
+                    print(replyCountNum,replyCountNum,likeCountNum)
+                    fetched_statistics = True
+                except:
+                    print("Cold'nt get reply, retweet, likes ")
+
+                            
                 # print(date,tm, is_a_tweet[0].text.replace('\n',''))
                 if (tweetText != None):
                     foundTweetCount += 1
@@ -42,6 +74,8 @@ def ScrapeTweets(driver: WebDriver,stop_condition):
                     new_tweet = {'username':username,'tweet_text':tweetText.text,'tweet_date':dateFromString.strftime("%Y-%m-%d %H:%M:%S")}
                     tweets_df = pd.concat([tweets_df,pd.DataFrame([new_tweet])],ignore_index = True)
                     
+            except selenium.common.exceptions.NoSuchElementException:
+                print("No such an element")
             except Exception:
                 print("An exception happened in parsing tweet")
             time.sleep(0.2)
